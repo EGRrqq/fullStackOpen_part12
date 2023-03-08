7ef93fc092087feb184cb15d19b8beb8067bb6fe
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-03-08 12:11:25 +0300

    ex 12.20 / create a production with all of the services

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3003',
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080/api/',
 })
 
 export default apiClient
